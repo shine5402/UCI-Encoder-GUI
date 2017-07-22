ffe39ce770718171253ad36a2c7193a44781feec
Bo <shine_5402@live.cn>
2017-07-22 17:27:03 +0800

    完善关于界面

--- unit2.pas
+++ unit2.pas
@@ -5,13 +5,18 @@ unit Unit2;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls;
 
 type
 
   { TForm2 }
 
   TForm2 = class(TForm)
+    Image1: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
     Memo1: TMemo;
     procedure Memo1Change(Sender: TObject);
   private
