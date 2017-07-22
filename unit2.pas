22becc88c5fa70bd045ae6d91a05fd7cf91262ed
Bo <shine_5402@live.cn>
2017-07-22 17:44:42 +0800

    修改关于，添加彩蛋

--- unit2.pas
+++ unit2.pas
@@ -14,10 +14,14 @@ type
 
   TForm2 = class(TForm)
     Image1: TImage;
+    Image2: TImage;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
     Memo1: TMemo;
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormCreate(Sender: TObject);
+    procedure Image1Click(Sender: TObject);
     procedure Memo1Change(Sender: TObject);
   private
     { private declarations }
@@ -27,7 +31,7 @@ type
 
 var
   Form2: TForm2;
-
+  t:integer;
 implementation
 
 {$R *.lfm}
@@ -37,6 +41,26 @@ implementation
 procedure TForm2.Memo1Change(Sender: TObject);
 begin
 
+end;
+
+procedure TForm2.Image1Click(Sender: TObject);
+begin
+t:=t+1;
+if t=10 then begin image1.Hide;
+image2.show;
+end;
+end;
+
+procedure TForm2.FormCreate(Sender: TObject);
+begin
+t:=0;
+end;
+
+procedure TForm2.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+t:=0;
+image1.show;
+image2.hide;
 end;
 
 end.
